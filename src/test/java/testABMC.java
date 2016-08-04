4b4ed6135761bc9e50ec52e2952159a80c26cc06
bruno <bisioba29@gmail.com>
2016-08-03 23:42:32 -0300

    cree clase de test

--- src/test/java/testABMC.java
+++ src/test/java/testABMC.java
@@ -1,3 +1,90 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class testABMC {
 	
+	@Before
+	public void init(){
+		
+	}
+	
+	@Test
+	public void altaBanco(){
+		
+	}
+	
+	@Test
+	public void altaCGP(){
+		
+	}
+	
+	@Test
+	public void altaLocalComercial(){
+		
+	}
+	
+	@Test
+	public void altaParadaColectivo(){
+		
+	}
+
+	@Test
+	public void modificarBanco(){
+		
+	}
+	
+	@Test
+	public void modificarCGP(){
+		
+	}
+	
+	@Test
+	public void modificarLocalComercial(){
+		
+	}
+	
+	@Test
+	public void modificarParadaColectivo(){
+		
+	}
+
+	@Test
+	public void borrarBanco(){
+		
+	}
+	
+	@Test
+	public void borrarCGP(){
+		
+	}
+	
+	@Test
+	public void borrarLocalComercial(){
+		
+	}
+	
+	@Test
+	public void borrarParadaColectivo(){
+		
+	}
+
+	@Test
+	public void consultarBanco(){
+		
+	}
+	
+	@Test
+	public void consultarCGP(){
+		
+	}
+	
+	@Test
+	public void consultarLocalComercial(){
+		
+	}
+	
+	@Test
+	public void consultarParadaColectivo(){
+		
+	}
 }
