e3fba40414229f9431b78ea372eebed4fe340e25
bruno <bisioba29@gmail.com>
2016-08-15 14:25:25 -0300

    lalala

--- src/main/java/DB/DB_HistorialBusquedas.java
+++ src/main/java/DB/DB_HistorialBusquedas.java
@@ -9,15 +9,15 @@ import POI.POI;
 
 public class DB_HistorialBusquedas {
 
-	private Map<Long,registroHistorico> listadoRegistros = new HashMap<Long,registroHistorico>();
-	
+	private Map<Long, registroHistorico> listadoRegistros = new HashMap<Long, registroHistorico>();
+
 	private static DB_HistorialBusquedas instance = null;
 
 	public DB_HistorialBusquedas() {
-		listadoRegistros = new HashMap<Long,registroHistorico>();
+		listadoRegistros = new HashMap<Long, registroHistorico>();
 	}
 
-	public Map<Long,registroHistorico> getListado() {
+	public Map<Long, registroHistorico> getListado() {
 		return instance.listadoRegistros;
 	}
 
@@ -27,18 +27,30 @@ public class DB_HistorialBusquedas {
 		return instance;
 	}
 
-	public void agregarHistorialBusqueda(registroHistorico registro){
+	public void agregarHistorialBusqueda(registroHistorico registro) {
 		String registroStr = Integer.toString(listadoRegistros.size());
-		listadoRegistros.put(Long.parseLong(registroStr),registro);
+		listadoRegistros.put(Long.parseLong(registroStr), registro);
 	}
 
-	// Reporte de busquedas por fecha y cantidad de todo el sistema	
+	// Reporte de busquedas por fecha y cantidad de todo el sistema
 	public Map<String, Long> reporteBusquedas(String criterioConsulta) {
 		Map<String, Long> resumen = new HashMap<String, Long>();
-		for(registroHistorico registro : registros)
-		
+		String fechaPrevia = "";
+		Long cantParcial = 0L;
+		for (Map.Entry<Long, registroHistorico> registro : listadoRegistros.entrySet()) {
+			String fechaActual = registro.getValue().getTime().toString();
+			if (fechaPrevia.equals(fechaActual)) {
+				cantParcial += registro.getValue().getCantResultados();
+			} else {
+				resumen.put(fechaPrevia, cantParcial);
+				fechaPrevia = fechaActual;
+				cantParcial = registro.getValue().getCantResultados();
+				
+			}
+
+		}
+
 		return resumen;
 	}
-	
-	
+
 }
--- src/main/java/DB/registroHistorico.java
+++ src/main/java/DB/registroHistorico.java
@@ -2,15 +2,52 @@ package DB;
 
 import java.sql.Timestamp;
 
-
 public class registroHistorico {
-	
-	Timestamp 	time;
-	long		userID;
-	String		busqueda;
-	long		cantResultados;
-	double		tiempoDeConsulta;
-	
-	
+
+	private Timestamp time;
+	private long userID;
+	private String busqueda;
+	private long cantResultados;
+	private double tiempoDeConsulta;
+
+	public Timestamp getTime() {
+		return time;
+	}
+
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+
+	public long getUserID() {
+		return userID;
+	}
+
+	public void setUserID(long userID) {
+		this.userID = userID;
+	}
+
+	public String getBusqueda() {
+		return busqueda;
+	}
+
+	public void setBusqueda(String busqueda) {
+		this.busqueda = busqueda;
+	}
+
+	public long getCantResultados() {
+		return cantResultados;
+	}
+
+	public void setCantResultados(long cantResultados) {
+		this.cantResultados = cantResultados;
+	}
+
+	public double getTiempoDeConsulta() {
+		return tiempoDeConsulta;
+	}
+
+	public void setTiempoDeConsulta(double tiempoDeConsulta) {
+		this.tiempoDeConsulta = tiempoDeConsulta;
+	}
 
 }
