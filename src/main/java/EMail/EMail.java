a6d62757e71f05f4d7c7a0173fa8f341383f6138
lag21392 <lag21392@gmail.com>
2016-08-15 22:37:48 -0300

    cosas para email en el Pom y nueva clase email

---
+++ src/main/java/EMail/EMail.java
@@ -0,0 +1,90 @@
+package EMail;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EMail {
+private final Properties properties = new Properties();
+	
+	private String password;
+ 
+	private Session session;
+	private String emailAdmin;
+	
+ 
+	
+	private void init() {
+ 
+		
+		
+		properties.put("mail.smtp.host", "mail.gmail.com");
+		properties.put("mail.smtp.starttls.enable", "true");
+		properties.put("mail.smtp.port",25);
+		properties.put("mail.smtp.mail.sender","emisor@gmail.com");
+		properties.put("mail.smtp.user", "usuario");
+		properties.put("mail.smtp.auth", "true");
+ 
+		session = Session.getDefaultInstance(properties);
+	}
+	
+	public void enviarEmailDemoraDeXSegundos(double segundos){
+		 
+		init();
+		String text="La busqueda demoro mas de "+segundos+" ";
+		String subject="Demora de busqueda";
+		try{
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress((String)properties.get("mail.smtp.mail.sender")));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress("receptor@gmail.com"));
+			message.setSubject(subject);
+			message.setText(text);
+			Transport t = session.getTransport("smtp");
+			t.connect((String)properties.get("mail.smtp.user"), "password");
+			t.sendMessage(message, message.getAllRecipients());
+			t.close();
+		}catch (MessagingException me){
+                        //Aqui se deberia o mostrar un mensaje de error o en lugar
+                        //de no hacer nada con la excepcion, lanzarla para que el modulo
+                        //superior la capture y avise al usuario con un popup, por ejemplo.
+			System.out.println("Error de envio de E-Mail");
+			return;
+		}
+		
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Session getSession() {
+		return session;
+	}
+
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	public String getEmailAdmin() {
+		return emailAdmin;
+	}
+
+	public void setEmailAdmin(String emailAdmin) {
+		this.emailAdmin = emailAdmin;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+	
+}
+
+
+
